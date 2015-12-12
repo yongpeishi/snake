7cefcf6418cfa1420b723305fcdd8a8a01d31e2a
yongpeishi <yongpeishi@gmail.com>
2015-12-12 22:01:38 +1100

    Have a rect that move in the direction of arrow key pressed

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var move = function() {};
+
 var SnakeSegment = React.createClass({
   getInitialState: function() {
     return {
@@ -7,18 +9,38 @@ var SnakeSegment = React.createClass({
   },
 
   changeDirection: function(e) {
-    console.log('haha');
+    var newState = {
+      'ArrowUp': function(currentX, currentY) {
+        newY = Math.max(currentY - 10, 0);
+        return {x: currentX, y: newY}
+      },
+      'ArrowDown': function(currentX, currentY) {
+        newY = Math.min(currentY + 10, 500-10);
+        return {x: currentX, y: newY}
+      },
+      'ArrowLeft': function(currentX, currentY) {
+        newX = Math.max(currentX - 10, 0);
+        return {x: newX, y: currentY}
+      },
+      'ArrowRight': function(currentX, currentY) {
+        newX = Math.min(currentX + 10, 500-10);
+        return {x: newX, y: currentY}
+      }
+    };
+
+    var that = this;
+    var pressedKey = e.key;
+    move = function() { that.setState(newState[pressedKey](that.state.x, that.state.y)); };
   },
 
   render: function() {
-    return React.createElement('svg', { 'width': '500', 'height': '500', tabIndex: '1', onKeyPress: this.changeDirection },
+    return React.createElement('svg', { 'width': '500', 'height': '500', tabIndex: '1', onKeyDown: this.changeDirection },
       React.createElement('rect',{
         x: this.state.x,
         y: this.state.y,
         width: 10,
         height: 10,
         stroke: 'black',
-        strokeWidth: 10,
         fill: 'black'
       })
   )}
@@ -29,3 +51,4 @@ ReactDOM.render(
   React.createElement(SnakeSegment), document.getElementById('field')
 );
 
+setInterval(function() { move() }, 1000);
